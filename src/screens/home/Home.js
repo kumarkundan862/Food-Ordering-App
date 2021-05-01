899b4768fad5c039ffba0548f5e73d8a7352e0a3
kumarkundan862 <kumarkundan862@gmail.com>
2021-05-01 08:50:06 +0530

    added packages of  material UI

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import './Home.css';
+import RestaurantCard from '../restaurantCard/RestaurantCard';
+import Header from '../../common/header/Header';
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                Home page
+            <div className="home-container">
+                <Header />
+                <div className="home-page">
+                    <RestaurantCard />
+                    <RestaurantCard />
+                    <RestaurantCard />
+                    <RestaurantCard />
+                    <RestaurantCard />
+                    <RestaurantCard />
+                    <RestaurantCard />
+                    <RestaurantCard />
+
+                </div>
             </div>
         );
     }
